ca2dff7882ed01796ed0514db64dd8ac686f3e19
Licancan <LCH327469441@gmail.com>
2020-06-19 15:46:50 +0800

    make it better

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 st.title("A svm-based model to predict coronary heart disease")
 
 gensini= st.sidebar.slider('gensini',0,300)
